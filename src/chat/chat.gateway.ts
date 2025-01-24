08a15942e475f38ad99cd862042b981b80a4344f
taewoo <rlaxodn322@gmail.com>
2025-01-24 15:36:01 +0900

    주석 처리 전

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -10,10 +10,13 @@ import {
 import { Server, Socket } from 'socket.io';
 import { AddMessageDto } from './dto/add-message.dto';
 import { Logger } from '@nestjs/common';
+
+// WebSocketGateway 데코레이터는 WebSocket 서버를 설정하며, CORS를 활성화하여 모든 도메인에서 접속 가능하도록 설정
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
-  server: Server;
+  server: Server; // socket.io 서버 객체
+  // NestJS Logger를 사용하여 로깅 기능을 제공
   private logger = new Logger('ChatGateway');
   @SubscribeMessage('chat')
   handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
