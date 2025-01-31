c11c4cc5f4c42fb28ee1de4c5967c02282c4f6d3
taewoo <rlaxodn322@gmail.com>
2025-01-31 10:34:03 +0900

    class-validator 추가

--- src/chat/dto/add-message.dto.ts
+++ src/chat/dto/add-message.dto.ts
@@ -1,4 +1,11 @@
+import { IsString } from 'class-validator';
+
 export class AddMessageDto {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
   author: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsString()
   body: string;
 }
